9d9210fd7b95ac39e3436f2d5d3ea3f3e3dca6b2
bruno.fecchio <bruno.fecchio@infojobs.com.br>
2024-06-28 12:30:26 -0300

    Initial commit

---
+++ src/ApplicantTracking.Api/Controllers/CandidateController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ApplicantTracking.Api.Controllers
+{
+    [ApiController]
+    public sealed class CandidateController : ControllerBase
+    {
+        [HttpGet("candidates")]
+        public async Task<IActionResult> List()
+        {
+            // TODO: Implement this method
+            return Ok();
+        }
+
+        [HttpGet("candidates/{idCandidate:int}")]
+        public async Task<IActionResult> Get([FromRoute] int idCandidate)
+        {
+            // TODO: Implement this method
+            return Ok();
+        }
+
+        // TODO: Change 'object candidate' to your viewmodel
+        [HttpPost("candidates")]
+        public async Task<IActionResult> Create([FromBody] object candidate)
+        {
+            // TODO: Implement this method
+            return Ok();
+        }
+
+        // TODO: Change 'object candidate' to your viewmodel
+        [HttpGet("candidates/{idCandidate:int}")]
+        public async Task<IActionResult> Edit([FromRoute] int idCandidate, [FromBody] object candidate)
+        {
+            // TODO: Implement this method
+            return NoContent();
+        }
+
+        [HttpDelete("candidates/{idCandidate:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int idCandidate)
+        {
+            // TODO: Implement this method
+            return NoContent();
+        }
+    }
+}
